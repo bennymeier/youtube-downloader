1a84add00a83a9ba8bcb25cb83eda5318ddd0e97
Benny <meier.benjamin@web.de>
2020-08-03 14:29:32 +0200

    Fix wrong hostname, forgot https

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,7 +1,7 @@
 const isLocalHost = window.location.hostname === "localhost";
 export const host = isLocalHost
   ? "http://localhost:4000"
-  : window.location.hostname;
+  : `https://${window.location.hostname}`;
 
 export const getDownloadUrl = (videoId, format = "mp4") =>
   `${host}/watch?v=${videoId}&format=${format}`;
